1a3cb8df30a7a1fbf72164292cad5acd4634fe3d
Ramiro Renteria <Ramis28@yahoo.com>
2023-09-13 18:30:41 -0700

    Cleaned up my code of old comments and unused files

--- src/components/Box.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-//This is for the box that's going to generate random colors
-// export default function Box(props) {
-//     return (
-//         <div className='box' style={{backgroundColor: `${props.color}`}}>
-//         </div>
-//     )
-// }
